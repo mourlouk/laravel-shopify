a04fea0a96f53c69d3693e870ff13966c7d91046
mourlouk <77194621+mourlouk@users.noreply.github.com>
2024-05-23 19:09:06 +0300

    Update default.blade.php
    Removed the ShopID argument from the appBridge app creation.

--- src/resources/views/layouts/default.blade.php
+++ src/resources/views/layouts/default.blade.php
@@ -17,9 +17,10 @@
             </div>
         </div>
 
-        @if(\Osiset\ShopifyApp\Util::getShopifyConfig('appbridge_enabled') && \Osiset\ShopifyApp\Util::useNativeAppBridge())
-            <script src="https://unpkg.com/@shopify/app-bridge{{ \Osiset\ShopifyApp\Util::getShopifyConfig('appbridge_version') ? '@'.config('shopify-app.appbridge_version') : '' }}"></script>
-            <script src="https://unpkg.com/@shopify/app-bridge-utils{{ \Osiset\ShopifyApp\Util::getShopifyConfig('appbridge_version') ? '@'.config('shopify-app.appbridge_version') : '' }}"></script>
+        @if(\Osiset\ShopifyApp\Util::getShopifyConfig('appbridge_enabled') && \Osiset\ShopifyApp\Util::useNativeAppBridge())    
+            <script src="https://unpkg.com/@shopify/app-bridge@3.5.1/umd/index.js"></script>
+            <script src="https://unpkg.com/@shopify/app-bridge-utils@3.5.1/umd/index.js"></script>
+    
             <script
                 @if(\Osiset\ShopifyApp\Util::getShopifyConfig('turbo_enabled'))
                     data-turbolinks-eval="false"
@@ -30,8 +31,7 @@
                 var utils = window['app-bridge-utils'];
                 var createApp = AppBridge.default;
                 var app = createApp({
-                    apiKey: "{{ \Osiset\ShopifyApp\Util::getShopifyConfig('api_key', $shopDomain ?? Auth::user()->name ) }}",
-                    shopOrigin: "{{ $shopDomain ?? Auth::user()->name }}",
+                    apiKey: "{{ \Osiset\ShopifyApp\Util::getShopifyConfig('api_key', $shopDomain ?? Auth::user()->name ) }}",                    
                     host: "{{ \Request::get('host') }}",
                     forceRedirect: true,
                 });
